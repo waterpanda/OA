c400430748562c0130dcc1df3246a47842a850ad
JiangLi <JL_raining@163.com>
2015-10-05 11:31:19 +0800

    更新

--- js/jl/addUser.js
+++ js/jl/addUser.js
@@ -2,44 +2,54 @@
  * Created by Administrator on 2015/9/26 0026.
  */
 $(function(){
+    init();
+    checkIsSame();
+    checkNull();
+    firstRegister();//第一次注册事件
+});
 
-})();
+//init
+function init(){
+    $("#commit-li").click(function(){
+        $("#commit-li").addClass("status-active");
+        $("#add-mes-li").removeClass("status-active");
+        $("#first-commit-user").show();
+        $("#second-add-mes").hide();
+    });
+    $("#add-mes-li").click(function(){
+        $("#commit-li").removeClass("status-active");
+        $("#add-mes-li").addClass("status-active");
+        $("#first-commit-user").hide();
+        $("#second-add-mes").show();
+    });
+}
+
 //检查两次输入密码是否相同
 function checkIsSame() {
-    $(".register #confirmPassword").blur(function () {
+    $("#first-commit-user #confirmPassword").blur(function () {
         if ($(this).val() != "") {
-            var val1 = $(".register #password").val();
+            var val1 = $("#first-commit-user #inputPassword").val();
             var val2 = $(this).val();
             if (val1 != val2) {
-                $(this).parents(".form-group").removeClass("has-success").addClass("has-warning");
-                $(this).next().removeClass("fa fa-check").addClass("fa fa-warning").css("color", "#8a6d3b");
-                $(this).parent().next().html("两次输入密码不一致！");
+                $(".confirmPassword-status").html("*两次输入密码不一致！");
             }
         }
     });
 }
+
 //输入框为空的检查函数
 function checkNull(){
-    for(var i=0;i<3;i++){
-        $(".form-group input").eq(i).blur(function(){
-            if($(this).val()==null || $(this).val()==""){
-                $(this).parents(".form-group").addClass("has-warning");
-                $(this).next().addClass("fa fa-warning").css("color","#8a6d3b");
-                $(this).parent().next().html("输入不能为空!");
-            }else{
-                $(this).parents(".form-group").addClass("has-success");
-                $(this).next().addClass("fa fa-check").css("color","#3c763d");
-            }
-        }).focus(function(){
-            $(this).parents(".form-group").removeClass("has-success has-warning");
-            $(this).next().removeClass("fa fa-check fa fa-warning");
-            $(this).parent().next().html("");
-        });
-    }
+    $("#first-commit-user form input").blur(function(){
+        var value = $(this).val();
+        if(isNull(value)){
+            $(this).parent().children("p").html("*输入不能为空！");
+        }
+    })
 }
+
 //注册按钮点击事件
 function firstRegister(){
-    $(".register-btn").click(function(){
+    $("#first-submit").click(function(){
         
     });
 }
